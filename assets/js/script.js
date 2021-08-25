e3e0172c24e5966fd61cb52400f2bc4415334df8
Scotty Yackzan <syackzan@gmail.com>
2021-08-25 16:10:08 -0700

    Added fetch request for weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
 console.log("hello");
+var button1 = document.getElementById("button1");
+
+function getApi(){
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d38851407b874b51cfa24d8ab452271a";
+    //https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=d38851407b874b51cfa24d8ab452271a
+    console.log(requestUrl);
+
+    fetch(requestUrl)
+        .then(function(response){
+            console.log(response);
+            
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data);
+    });
+}
+
+button1.addEventListener("click", getApi)
