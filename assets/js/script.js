a53198ef7feb21e40d40c4b8e0b1684aea074081
Scotty Yackzan <syackzan@gmail.com>
2021-08-25 17:55:58 -0700

    Added search Functionality into the Website + so selected City comes up on weather Dashboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,14 @@ var wind = document.getElementById("windN");
 var humidityN = document.getElementById("humidityN");
 var uvIndexN = document.getElementById("uvIN");
 
+var placeholder = "Seattle";
 
-function getApi(){
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=Atlanta,us&APPID=d38851407b874b51cfa24d8ab452271a";
+var dateN = moment().format("MMMM Do YYYY");
+console.log(dateN);
+
+
+function getApi(inputStorage){
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + inputStorage +",us&units=imperial&APPID=d38851407b874b51cfa24d8ab452271a";
     console.log(requestUrl);
 
     fetch(requestUrl)
@@ -20,14 +25,21 @@ function getApi(){
         })
         .then(function(data){
             console.log(data);
+            
+            city.textContent = data.name + " (" + dateN + ")";
+            temp.textContent = "Temp: " + data.main.temp + "°F";
+            wind.textContent = "Wind: " + data.wind.speed + "mph";
+            humidityN.textContent = "Humidity: " + data.main.humidity + "%";
+            //uvIndexN.textContent = 
     });
 }
 
-getApi();
+getApi(placeholder);
 
 function saveSearch(){
     var inputStorage = inputS.value;
     console.log(inputStorage);
+    getApi(inputStorage);
 }
 
 buttonS.addEventListener("click", saveSearch);
